355f94cfcb2e42ea7d7ca31b3dbc9cdf04c285e0
BakrimNadia <nadia_lina@hotmail.com>
2025-01-17 12:02:38 +0100

    presentation

---
+++ src/app/Components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+        
+    <nav className="navbar">
+      <ul>
+        <li><a href="#hero">Accueil</a></li>
+        <li><a href="#about">À propos</a></li>
+        <li><a href="#projects">Projets</a></li>
+        <li><a href="#skills">Compétences</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+    </div>
+  );
+}
+
+export default Navbar;
