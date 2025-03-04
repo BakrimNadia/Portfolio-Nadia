3d431160cb3922f71f7220563e677875c44e445b
BakrimNadia <nadia_lina@hotmail.com>
2025-03-04 23:25:58 +0100

    fix: rectify input name to contact form

--- src/app/Components/Contact.tsx
+++ src/app/Components/Contact.tsx
@@ -69,7 +69,7 @@ export default function Contact() {
               <input
                 required
                 id="first-name"
-                name="first-name"
+                name="firstName"
                 type="text"
                 autoComplete="given-name"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-rose-600"
@@ -86,7 +86,7 @@ export default function Contact() {
               <input
                 required
                 id="last-name"
-                name="last-name"
+                name="lastName"
                 type="text"
                 autoComplete="family-name"
                 className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-rose-600"
@@ -136,7 +136,7 @@ export default function Contact() {
               <div className="flex rounded-md bg-white outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-rose-600">
                 <input
                   id="phone-number"
-                  name="phone-number"
+                  name="phoneNumber"
                   type="text"
                   placeholder=""
                   className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
